374d609fe5ea6c96e54fef0919e3c241788f9eda
TunaWeiWei <somacruz23759139@gmail.com>
2023-10-03 18:54:11 +0800

    Initial commit

---
+++ src/main/java/com/web/billy/ShopingWebPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.web.billy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopingWebPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopingWebPracticeApplication.class, args);
+	}
+
+}
