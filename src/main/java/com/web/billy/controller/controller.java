d5d8c0f2225ddd9bc48c57508c420971dd22cb3a
TunaWeiWei <somacruz23759139@gmail.com>
2023-10-10 00:32:38 +0800

    10/9

--- src/main/java/com/web/billy/controller/controller.java
+++ src/main/java/com/web/billy/controller/controller.java
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class controller {
 			 
-	@GetMapping("/")
+	@GetMapping({"/", "/index"})
 	public String index() {
 		return"index";
+	}
+	
+	@GetMapping("/productIntro")
+    public String productIntro() {
+        return "productIntro";
+	}
+	
+	@GetMapping("/buyingPage")
+    public String buyingPage() {
+        return "buyingPage";
 	}	
 }
